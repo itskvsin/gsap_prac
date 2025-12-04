3b5a90c4838383fed8ddbeb096f1bb92713804a9
itskvsin <kevinsolankistudy@gmail.com>
2025-12-04 16:21:18 +0530

    Initialized the repo

---
+++ components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+        <div id='videoFrame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+            <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg '>
+                mini videoplayer
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+
+function App() {
+  return (
+    <>
+      <main className="min-h-screen relative overflow-x-hidden w-screen">
+        <Hero />
+      </main>
+    </>
+  );
+}
+
+export default App;
