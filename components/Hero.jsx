db2ab452df2227986766ccbe0187e46609cf8adc
itskvsin <kevinsolankistudy@gmail.com>
2025-12-05 17:13:11 +0530

    completed hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,170 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import {ScrollTrigger} from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
-  return (
-    <div className='relative h-dvh w-screen overflow-x-hidden'>
-        <div id='videoFrame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
-            <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg '>
-                mini videoplayer
-            </div>
-        </div>
-    </div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
 
-export default Hero
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const miniVdClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos -1) {
+        setIsLoading(false)
+    }
+  }, [loadedVideos])
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(20% 0%, 80% 0%, 92% 82%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0, 100% 0, 100% 100%, 0 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+        {
+            isLoading && (
+                <div  className="flex-center absolute z-[10] h-dvh w-screen overflow-hidden bg-voilet-50">
+                    <div className="three-body">
+                        <div className="three-body__dot"></div>
+                        <div className="three-body__dot"></div>
+                        <div className="three-body__dot"></div>
+                    </div>
+                </div>
+            )
+        }
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg ">
+            <div
+              onClick={miniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:opacity-100 hover:scale-100 "
+            >
+              <video
+                src={getVideoSrc(upcomingVideoIndex)}
+                muted
+                loop
+                id="curremt-video"
+                className="size-64 origin-center scale-150 object-cover object-center "
+                onLoadedData={handleVideoLoad}
+                ref={nextVideoRef}
+              />
+            </div>
+          </div>
+          <video
+            ref={nextVideoRef}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center z-20 invisible size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+            src={getVideoSrc(upcomingVideoIndex)}
+          />
+
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+              autoPlay
+            loop
+            muted
+            className="absolute top-0 left-0 size-full object-cover object-center "
+          />
+        </div>
+
+        <h1 className="special-font hero-heading  absolute bottom-5 right-5 z-40 text-blue-50">
+          G<b>A</b>MING
+        </h1>
+
+        <div className="absolute  left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10 ">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefin <b>n</b>e
+            </h1>
+
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the metagame layer <br /> unleash the play economy
+            </p>
+
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="bg-yellow-300 flex-center gap-1"
+            />
+          </div>
+        </div>
+      </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>A</b>MING
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
     <>
       <main className="min-h-screen relative overflow-x-hidden w-screen">
         <Hero />
+
+        <section className="relative min-h-screen w-screen overflow-x-hidden"/>
       </main>
     </>
   );
